01a2b4632bcfd986d7d250ee8ea4ef8edd795541
Schmokes <lesa22kz@student.ju.se>
2022-12-09 10:57:17 +0100

    favorites heart

--- heart.js
+++ heart.js
@@ -1,30 +1,30 @@
+let isHeartFull = false;
+
+window.addEventListener('load', function () {
 const button = document.getElementById("bu-heart");
 const filledHeartIcon = document.getElementById("fa-heart-filled");
 const emptyHeartIcon = document.getElementById("fa-heart-empty")
 console.log(button);
 
-
-/* The fav heart will change fa-icon, marked as added to fav */
-/* button.addEventListener('load'), () => loadButton()  */
-
-if (emptyHeartIcon.style.display = 'block') {
-    button.addEventListener('click',
-    function changeHeart() {
+// clicking the fav button
+button.addEventListener('click', function() {
+    // checking if added to fav
+    if (isHeartFull === true) {
+        // emptying it - removing from fav
+        console.log("heart empty");
+        emptyHeartIcon.style.display = 'block';
+        filledHeartIcon.style.display = 'none';
+        isHeartFull = false;
+    
+    }
+    else {
+        // filling it - adding to fav
         console.log("heart fill");
         emptyHeartIcon.style.display = 'none';
         filledHeartIcon.style.display = 'block';
-    })
-}
-else if (emptyHeartIcon.style.display = 'none'){
-    button.addEventListener('click',
-    function changeHeart() {
-        console.log("heart empty");
-        emptyHeartIcon.style.display = 'block';
-        filledHeartIcon.style.display = 'none'
-    })
-}
 
-
-
-
-
+        isHeartFull = true;
+       
+    }
+})
+});
